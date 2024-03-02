e0aabbdcfdde84699dcb5374e18656f5c16ed2c1
Andre-G-C-Araujo <AndreCaue>
2024-03-02 12:31:23 -0300

    prettier plugins configs and adds

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/ui/button";
 
 export const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <Button>Entrar</Button>
     </div>
   );
