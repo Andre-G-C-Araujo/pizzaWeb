2f3b403c35fe405b9cb5ca322bc1d93528616114
Andre-G-C-Araujo <AndreCaue>
2024-03-02 11:57:53 -0300

    shadcn ui configs and install

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+import "./global.css";
+
 export const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
+  );
 };
